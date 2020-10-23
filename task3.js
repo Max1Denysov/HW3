ad95a706cb104bf7f0872dda1c0310acd2c35013
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-10-23 21:32:55 +0300

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,3 +1,10 @@
 function task3(w){
     return Math.min(...w.split(" ").map (w => w.length));
 }
+
+//-------------------------------------------------------------
+function task3(w) {
+   let res1 = Math.min(...w.split(' ').map (w => w.length));
+   alert(res1)  
+}
+task3('lets talk about javascript the best language')
