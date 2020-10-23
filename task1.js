af752c47cdf9f0eec6ccd0fd62f26738cdd83e88
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-10-23 21:48:32 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -2,3 +2,9 @@ function task1(arr) {
   return arr.map(v => Math.min(...v)).reduce((a , b) => a + b , 0)
 }
 
+//----------------------------------------------------------------------
+function task1(arr) {
+ let res = arr.map(v => Math.min(...v)).reduce((a , b) => a + b , 0)
+ document.write(res)
+}
+task1([[1,2,3,4,5],[5,6,7,8,9],[20,21,34,56,100]])
