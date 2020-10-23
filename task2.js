ddf211725e3df05739a6a4f869b4a6cccead3082
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-10-23 21:40:17 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -7,4 +7,10 @@ const task2 = (ages) => ages.sort( (a , b) => a - b).slice(-2)
 function task2(ages){
 return ages.sort( (a , b) => a - b).slice(-2)
 }
+//
+function task2(ages){
+let res= ages.sort( (a , b) => a - b).slice(-2)
+alert(res) 
+}
+task2([1,2,10,8]) 
 
