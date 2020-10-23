27c99fed1b05c132f3a693ecff90b51eea34bdf2
Max1Denysov <72945727+Max1Denysov@users.noreply.github.com>
2020-10-23 13:58:20 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -1,7 +1,10 @@
-  const arr = [
+[
   [1, 2, 3, 4, 5],  
   [5, 6, 7, 8, 9],  
   [20, 21, 34, 56, 100]  
 ]
-  alert(arr.map(v=>Math.min(...v)).reduce((a , b)=> a + b,0))   
  
+//
+function sumOfMinimums(arr) {
+  return arr.map(v=>Math.min(...v)).reduce((a,b)=>a+b,0)
+}
